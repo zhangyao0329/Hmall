d4cb8b8a0d06aaa1b6d8e341af34ef3d3728bfc2
张遥 <1870476411@qq.com>
2025-07-06 19:01:02 +0800

    完整五个功能模块的拆分
    用户、交易、支付、商品、购物车

--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -2,8 +2,11 @@ package com.hmall.api.client;
 
 
 import com.hmall.api.dto.ItemDTO;
+import com.hmall.api.dto.OrderDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
@@ -16,5 +19,10 @@ public interface ItemClient {
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
     // discoveryClient.getInstances("item-service").get(RandomUtil.randomInt(instances.size())).getUri()
     // /items?ids=1,2,3
+
+
+    @PutMapping("/items/stock/deduct")
+    void deductStock(@RequestBody List<OrderDetailDTO> items);
+
 
 }
--- item-service/src/main/java/com/hmall/item/controller/ItemController.java
+++ item-service/src/main/java/com/hmall/item/controller/ItemController.java
@@ -2,11 +2,12 @@ package com.hmall.item.controller;
 
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmall.api.dto.ItemDTO;
+import com.hmall.api.dto.OrderDetailDTO;
 import com.hmall.common.domain.PageDTO;
 import com.hmall.common.domain.PageQuery;
 import com.hmall.common.utils.BeanUtils;
-import com.hmall.item.domain.dto.ItemDTO;
-import com.hmall.item.domain.dto.OrderDetailDTO;
+
 import com.hmall.item.domain.po.Item;
 import com.hmall.item.service.IItemService;
 import io.swagger.annotations.Api;
@@ -36,6 +37,7 @@ public class ItemController {
     @ApiOperation("根据id批量查询商品")
     @GetMapping
     public List<ItemDTO> queryItemByIds(@RequestParam("ids") List<Long> ids){
+
         return itemService.queryItemByIds(ids);
     }
 
